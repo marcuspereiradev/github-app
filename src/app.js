6be0bdb7c9b985e24df42637032cef77be86cb04
marcuspereiradev <vinnycoast@gmail.com>
2019-06-11 18:26:11 -0300

    Creating app marking

---
+++ src/app.js
@@ -0,0 +1,42 @@
+'use strict'
+
+import React from 'react'
+
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input type='search' placeholder='Digite o nome do usuário no GitHub' />
+    </div>
+    <div className='user-info'>
+      <img src='https://avatars1.githubusercontent.com/u/30603440?v=4'></img>
+      <h1><a href='https://github.com/marcuspereiradev'>Marcus Pereira</a></h1>
+
+      <ul className='respos-info'>
+        <li>Repositórios: 74</li>
+        <li>Seguidores: 2</li>
+        <li>Seguindo: 2</li>
+      </ul>
+
+      <div className='actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
+      </div>
+
+      <div className='repos'>
+        <h2>Repositórios:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
