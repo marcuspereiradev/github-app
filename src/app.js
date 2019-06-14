b6bff0f6cdf84175146317d0949994339e1ab9cb
marcuspereiradev <vinnycoast@gmail.com>
2019-06-14 14:36:15 -0300

    Create repos component

--- src/app.js
+++ src/app.js
@@ -4,22 +4,14 @@ import React from 'react'
 import Search from './components/search'
 import UserInfo from './components/user-info'
 import Actions from './components/actions'
+import Repos from './components/repos';
 
 const App = () => (
   <div className='app'>
     <Search />
     <UserInfo />
     <Actions />
-
-    
-
-      <div className='repos'>
-        <h2>Repositórios:</h2>
-        <ul>
-          <li><a href='#'>Nome do repositório</a></li>
-        </ul>
-      </div>
-
+    <Repos className='repos' />
       <div className='starred'>
         <h2>Favoritos:</h2>
         <ul>
---
+++ src/components/repos.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+
+const Repos = ({ className }) => (
+  <div className={className}>
+    <h2>Repositórios:</h2>
+    <ul>
+      <li><a href='#'>Nome do repositório</a></li>
+    </ul>
+  </div>
+)
+
+export default Repos
