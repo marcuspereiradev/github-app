27bf0dcd08de7a94f94c0413811fdb2517ce8887
marcuspereiradev <vinnycoast@gmail.com>
2019-06-14 14:25:21 -0300

    Create user-info component

--- src/app.js
+++ src/app.js
@@ -2,20 +2,12 @@
 
 import React from 'react'
 import Search from './components/search'
+import UserInfo from './components/user-info';
 
 const App = () => (
   <div className='app'>
     <Search />
-    <div className='user-info'>
-      <img src='https://avatars1.githubusercontent.com/u/30603440?v=4'></img>
-      <h1><a href='https://github.com/marcuspereiradev' target='_blank'>Marcus Pereira</a></h1>
-
-      <ul className='respos-info'>
-        <li>Repositórios: 74</li>
-        <li>Seguidores: 2</li>
-        <li>Seguindo: 2</li>
-      </ul>
-    </div>
+    <UserInfo />
 
     <div className='actions'>
         <button>Ver repositórios</button>
---
+++ src/components/user-info.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars1.githubusercontent.com/u/30603440?v=4'></img>
+    <h1><a href='https://github.com/marcuspereiradev' target='_blank'>Marcus Pereira</a></h1>
+
+    <ul className='respos-info'>
+      <li>Repositórios: 74</li>
+      <li>Seguidores: 2</li>
+      <li>Seguindo: 2</li>
+    </ul>
+  </div>
+)
+
+export default UserInfo
