e89e763cec03c4982200ed6531276a30b18f5681
marcuspereiradev <vinnycoast@gmail.com>
2019-07-03 14:04:28 -0300

    Add stylesheets

--- src/components/user-info.js
+++ src/components/user-info.js
@@ -5,13 +5,15 @@ import React from 'react'
 const UserInfo = () => (
   <div className='user-info'>
     <img src='https://avatars1.githubusercontent.com/u/30603440?v=4'></img>
-    <h1><a href='https://github.com/marcuspereiradev' target='_blank'>Marcus Pereira</a></h1>
+    <div className='user-info-content'>
+      <h1><a href='https://github.com/marcuspereiradev' target='_blank'>Marcus Pereira</a></h1>
 
-    <ul className='respos-info'>
-      <li>Repositórios: 74</li>
-      <li>Seguidores: 2</li>
-      <li>Seguindo: 2</li>
-    </ul>
+      <ul className='respos-info'>
+        <li>Repositórios: 74</li>
+        <li>Seguidores: 2</li>
+        <li>Seguindo: 2</li>
+      </ul>
+    </div>
   </div>
 )
 
